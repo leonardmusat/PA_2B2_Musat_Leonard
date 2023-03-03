5c16b13b6a5a27a5ed0740e96c7c07f0d5545d52
leonardmusat <79160780+leonardmusat@users.noreply.github.com>
2023-03-03 09:19:29 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+    /**
+     * Here the latin matrix is created.
+     */
+    static void createMatrix(int n, int[][] matrix) {
+        int count = 1;
+        int proCount = 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = count;
+                count++;
+                if (matrix[i][j] > n) {
+                    matrix[i][j] = matrix[i][j] - n;
+                }
+            }
+            count = proCount;
+            proCount++;
+        }
+    }
+
+    /**
+     * Displaying the matrix, no requirements in this direction, for verification only
+     */
+    static void displayMatrix(int[][] matrix) {
+        for (int k = 0; k < matrix.length; k++) { //this equals to the row in our matrix.
+            for (int l = 0; l < matrix[k].length; l++) { //this equals to the column in each row.
+                System.out.print(matrix[k][l] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Displaying rows of the matrix
+     */
+    static void displayRows(int[][] matrix) {
+        List<Integer> listRows = new ArrayList<>();
+        int count = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                listRows.add(matrix[i][j]);
+            }
+
+            System.out.println("Row with number " + count + ": " + listRows);
+            count++;
+            listRows.clear();
+        }
+    }
+
+    /**
+     * Displaying columns of the matrix
+     */
+    static void displayColumns(int[][] matrix) {
+        int count = 1;
+        List<Integer> listColumns = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                listColumns.add(matrix[j][i]);
+            }
+
+            System.out.println("Colum with number " + count + ": " + listColumns);
+            count++;
+            listColumns.clear();
+        }
+    }
+
+    public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
+        //int n = Integer.parseInt(args[0]);
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter n:");
+        n = scan.nextInt();
+        int[][] matrix = new int[n][n];
+
+        if (n > 1000) {
+            createMatrix(n, matrix);
+            displayRows(matrix);
+            displayColumns(matrix);
+            long t2 = System.currentTimeMillis();
+            System.out.println("The running time of the application in milliseconds is: " + (t2 - t1));
+
+        } else {
+            createMatrix(n, matrix);
+
+            displayMatrix(matrix);
+
+            displayRows(matrix);
+
+            displayColumns(matrix);
+        }
+    }
+}
