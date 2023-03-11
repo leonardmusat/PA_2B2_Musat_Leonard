a67ee1d140cee5f8588f3d6dfbf4ccedf6f3a3bf
leonardmusat <79160780+leonardmusat@users.noreply.github.com>
2023-03-11 18:56:37 +0200

    Add files via upload

---
+++ Homework2_PA/Person.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Person implements Comparable<Person>, Node {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
